51d9e3f95210314e6cfcb82b80c32b1f2dcb3ba5
cktang88 <arctangent20@yahoo.com>
2018-08-28 17:25:37 -0500

    Add auto-updating url

--- node-nfc/main.js
+++ node-nfc/main.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const port = 3000;
 const app = express();
-app.get('/url', function() {
 
+let fake_qr_data = []
+app.get('/', function (req, res) {
+    return res.json(fake_qr_data);
 });
 app.listen(port, function () {
     console.log("Server is running on " + port + " port");
 });
+
+setInterval(function () {
+    fake_qr_data.push(getRandomInt(10000000,99999999))
+}, 5000);
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
