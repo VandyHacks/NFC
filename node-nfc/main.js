0d40f1aef1c79d76e87ce0860441a9fa2a4d6a7e
cktang88 <arctangent20@yahoo.com>
2018-08-28 17:31:16 -0500

    Add comments, serve on /qrdata

--- node-nfc/main.js
+++ node-nfc/main.js
@@ -2,18 +2,23 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
+// fake qr data
 let fake_qr_data = []
-app.get('/', function (req, res) {
+// this endpoint should constantly update (serve json)
+app.get('/qrdata', function (req, res) {
     return res.json(fake_qr_data);
 });
+// server live on PORT number (eg. localhost:3000/)
 app.listen(port, function () {
     console.log("Server is running on " + port + " port");
 });
 
+// generates a fake qr unique id every 5 seconds
 setInterval(function () {
     fake_qr_data.push(getRandomInt(10000000,99999999))
 }, 5000);
 
+// generate random int in range
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
