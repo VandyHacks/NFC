0875568ead8ca32adeb0f190d73d0e88e7903739
Tim Liang <timothy.liang@vanderbilt.edu>
2018-09-05 08:47:39 -0500

    added basic mongodb support to python script

--- python-nfc/ioWorker.py
+++ python-nfc/ioWorker.py
@@ -1,6 +1,8 @@
 import threading
 import urllib.request
 import json
+import pymongo
+from pymongo import MongoClient
 
 # 1. Checks for endpoint to have new QR code
 # 2. Prompts for new NFC UID to associate with QR code.
@@ -8,32 +10,49 @@ import json
 # 3. Load json and validate that the QR code and NFC UID haven't been added.
 # 4. Write the new pair into the json
 
-def getNFCforQR(qrcode): 
-    with open("qr-nfc-pairs.json", mode = "r") as jsonfile:
-        jsondata = json.load(jsonfile) 
-        nfcUID = input("\nPlease scan NFC tag\n")
-        while len(nfcUID) != 14:
-            nfcUID = input("Invalid NFC tag, please rescan\n")
+#TODO - Implement a way to actually quit
 
-        for key in jsondata:
-            if key == qrcode:
-                print("ERROR: Found duplicate QR code\n")
-                return
-            elif jsondata[key] == nfcUID:
-                print("ERROR: Found duplicate NFC UID\n")
-                response = input("Try another tag (y/n)?\n")
-                if response != 'n':
-                    getNFCforQR(qrcode)
-                print("WARNING: QR code %s was not associated with NFC\n" % qrcode)
-                return
-        
-    with open("qr-nfc-pairs.json", mode = "w") as jsonfile:
-        jsondata[qrcode] = nfcUID
-        json.dump(jsondata, jsonfile)
-        print("Wrote new qr-nfc pair to json\n")
+def init():
+    #eventid = urllib.request.urlopen("http://localhost:3000/eventid").read().decode("utf-8")
+    eventid = 1 
+    if eventid == 0:
+        pollQrData()
+    else:
+        markAttendance(eventid)
+
+def getDb():
+    connection = MongoClient("ds231720.mlab.com", 31720)
+    db = connection['test-db']
+    db.authenticate('testuser', 'testpassword1')
+    return db
+
+def getNFCforQR(qrcode): 
+    db = getDb()
+    nfcUID = input("\nPlease scan NFC tag\n")
+    while len(nfcUID) != 14:
+        nfcUID = input("Invalid NFC tag, please rescan\n")
+
+    if db['studentID-nfcUID'].find_one( {'studentID': {'$eq': qrcode}} ) != None:
+        print("ERROR: Found duplicate QR code\n")
+        return
+
+    while db['studentID-nfcUID'].find_one( {'_id' : {'$eq': nfcUID}} ) != None:
+        print("ERROR: Found duplicate NFC UID\n")
+        response = input("Try another tag (y/n)?\n")
+        if response != 'n':
+            nfcUID = input("\nPlease scan NFC tag\n")
+        else:
+            print("WARNING: QR code %s was not associated with NFC\n" % qrcode)
+        return
+
+    db = getDb()
+    db['studentID-nfcUID'].insert( {"_id": nfcUID, "studentID": qrcode} )
+
+    # TODO - probably actually verify this
+    print("\n Successfully asssociated student and NFC UID\n")
 
 prev = ""
-def poll():
+def pollQrData():
     global prev
     print("Polling")
     contents = urllib.request.urlopen("http://localhost:3000/qrdata").read().decode("utf-8")
@@ -49,6 +68,23 @@ def poll():
         print(qrcode)
         getNFCforQR(qrcode)
 
-    threading.Timer(1.0, poll).start()
-    
-poll()
+    threading.Timer(1.0, pollQrData).start()
+
+def markAttendance(eventid):
+    db = getDb()
+    while True:
+        nfcUID = input("\nPlease scan NFC tag\n")
+        while len(nfcUID) != 14:
+            nfcUID = input("Invalid NFC tag, please rescan\n")
+        entry = db['studentID-nfcUID'].find_one( {'_id' : {'$eq': nfcUID}})
+        if entry == None:
+            print("Could not find student for NFC uid")
+            continue
+
+        studentID = entry['studentID']
+        db['events'].update( {'_id': 'TEST_EVENT'}, {'$push': {'attendees': studentID}} )
+
+        #TODO - verify 
+        print("Marked attended")
+        
+init()
