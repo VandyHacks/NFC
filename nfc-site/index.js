b64ee0fefc57d654c745d39e7bfbfb995339fb35
timothy-liang <timothy.liang@vanderbilt.edu>
2018-10-27 17:22:15 -0500

    fixed whitespace

--- nfc-site/index.js
+++ nfc-site/index.js
@@ -378,9 +378,7 @@ function displayUsers(json) {
     let entry = document.createElement("div")
     dom("#student-info").appendChild(entry)
     entry.className = "user-entry"
-    let text = `Name:\t${user.name}\n\
-                School:\t${user.school}\n\
-                Email:\t${user.email}\n`
+    let text = `\tName:\t${user.name}\n\tSchool:\t${user.school}\n\tEmail:\t${user.email}\n`
     entry.textContent = text;
   });
 }
